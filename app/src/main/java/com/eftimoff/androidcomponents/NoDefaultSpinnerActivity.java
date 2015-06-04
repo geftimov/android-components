2f2fe2399f960324a79b947ea66aebad9e9e64ec
Georgi Eftimov <georgi.eftimov@softwaregroup-bg.com>
2015-06-04 10:12:51 +0300

    Added NoDefaultSpinner.

---
+++ app/src/main/java/com/eftimoff/androidcomponents/NoDefaultSpinnerActivity.java
@@ -0,0 +1,13 @@
+package com.eftimoff.androidcomponents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoDefaultSpinnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_default_spinner);
+    }
+}
